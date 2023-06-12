f2c2603f19cf2d124cc40fb9bcafcee3d051167e
ahincho <ahincho@unsa.edu.pe>
2023-06-12 14:48:33 -0500

    Se cambia el Archivo WAR por Calculadora

--- calculadora/src/main/java/com/unsa/calculadora/HelloServlet.java
+++ calculadora/src/main/java/com/unsa/calculadora/HelloServlet.java
@@ -1,18 +1,15 @@
-package com.unsa.matriculas;
+package com.unsa.calculadora;
 
 import java.io.*;
-
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
 @WebServlet(name = "helloServlet", value = "/hello-servlet")
 public class HelloServlet extends HttpServlet {
     private String message;
-
     public void init() {
         message = "Hello World!";
     }
-
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
         // Hello
@@ -21,7 +18,6 @@ public class HelloServlet extends HttpServlet {
         out.println("<h1>" + message + "</h1>");
         out.println("</body></html>");
     }
-
     public void destroy() {
     }
 }
